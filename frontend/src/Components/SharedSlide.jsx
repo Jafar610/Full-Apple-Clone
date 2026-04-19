33dadecaefc211632e6810f871fb813a69f2568f
Jafar610 <jafarmohamed955@gmail.com>
2026-04-19 21:05:46 +0300

    eleventh commit

---
+++ frontend/src/Components/SharedSlide.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Slide from '../Pages/Slide/Slide'
+
+function SharedSlide() {
+  return (
+    <>
+      <Slide/>
+      
+    </>
+  )
+}
+
+export default SharedSlide
--- frontend/src/Pages/Apple_Store/Store.jsx
+++ frontend/src/Pages/Apple_Store/Store.jsx
@@ -1,31 +1,35 @@
-import React from 'react'
-import iphone from '../../assets/img/products/iphone 15Pro.png'
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper";
+import { Autoplay } from "swiper/modules";
+
 function Store() {
   return (
     <>
-     <div className='flex justify-center items-center w-full bg-white'>
-        <div className='flex justify-around items-center bg-gray-200  w-full'>
-            <div className='text-center'>
-                <h1 className='text-6xl font-bold font-poppins pt-3 pb-3 '>iPhone 15 Pro</h1>
-                <p className='text-3xl font-poppins font-meduim mt-2 mb-2'>Titanum, So strong. <br />
-                So light, So Pro.</p>
+     <div className="text-center">
+        <div>
+            <h1>Popular Product Catagories</h1>
+        </div>
 
-                <p className='text-3xl font-poppins '><span className='line-through text-2xl text-gray-800 mr-5'>$1,199</span> $1,099</p>
-
-
-                <div className='flex justify-center items-center mt-5'>
-                    <button className='border rounded-full px-10 py-2 mr-3 bg-blue-700 text-white text-md'>Buy</button>
-                    <button className='border rounded-full px-8 py-2 text-md hover:bg-blue-700 hover:text-white'>Learn More</button>
-                </div>
+        <div className="flex justify-center items-center">
+            <div>
+                <img src="" alt="" />
+                <h1>iMac</h1>
             </div>
 
             <div>
-                <img src={iphone} alt="" className='w-100 h-100' />
+                <img src="" alt="" />
+                <h1>iMac</h1>
+            </div>
+
+            <div>
+                <img src="" alt="" />
+                <h1>iMac</h1>
             </div>
         </div>
      </div>
     </>
-  )
+  );
 }
 
-export default Store
+export default Store;
