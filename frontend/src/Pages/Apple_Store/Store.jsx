f64beb3889064e3207d96115facdbb84c19445f0
Jafar610 <jafarmohamed955@gmail.com>
2026-04-20 14:29:20 +0300

    12th Update

--- frontend/src/Pages/Apple_Store/Store.jsx
+++ frontend/src/Pages/Apple_Store/Store.jsx
@@ -6,28 +6,54 @@ import { Autoplay } from "swiper/modules";
 function Store() {
   return (
     <>
-     <div className="text-center">
+      <div className="text-center mt-10">
         <div>
-            <h1>Popular Product Catagories</h1>
+          <h1>Popular Product Catagories</h1>
         </div>
 
-        <div className="flex justify-center items-center">
-            <div>
-                <img src="" alt="" />
-                <h1>iMac</h1>
-            </div>
+        <div className="flex justify-space-around items-center">
+          <Swiper
+            modules={[Autoplay]}
+            slidesPerView={3}
+            spaceBetween={10}
+            loop={true}
+            autoplay={{
+              delay: 3000, // 3 seconds
+              disableOnInteraction: false,
+            }}
+          >
 
-            <div>
+            <SwiperSlide>
+              <div>
                 <img src="" alt="" />
                 <h1>iMac</h1>
-            </div>
+              </div>
+            </SwiperSlide>
 
-            <div>
+            <SwiperSlide>
+              <div>
                 <img src="" alt="" />
                 <h1>iMac</h1>
-            </div>
+              </div>
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <div>
+                <img src="" alt="" />
+                <h1>iMac</h1>
+              </div>
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <div>
+                <img src="" alt="" />
+                <h1>iMac</h1>
+              </div>
+            </SwiperSlide>
+
+          </Swiper>
         </div>
-     </div>
+      </div>
     </>
   );
 }
