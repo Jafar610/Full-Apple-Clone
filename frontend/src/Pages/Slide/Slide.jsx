6ce14ffac141b6bf7300055c06d9be903351db84
Jafar610 <jafarmohamed955@gmail.com>
2026-04-28 01:26:26 +0300

    20th commit

--- frontend/src/Pages/Slide/Slide.jsx
+++ frontend/src/Pages/Slide/Slide.jsx
@@ -3,11 +3,16 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Autoplay } from "swiper/modules";
 import {useState, useEffect} from 'react';
-import {useNavigation} from 'react-router-dom';
 
 function Slide() {
-  const navigate = useNavigation();
-  
+  const [slides, setSlides] = useState([]);
+  useEffect(()=>{
+    fetch('http://localhost:3002/slide')
+    .then(res=> res.json())
+    .then(data=>{
+      setSlides(data);
+    })
+  },[])
 
 
 
@@ -24,71 +29,37 @@ function Slide() {
           }}
           modules={[Autoplay]}
         >
-          <SwiperSlide>
+          {slides.map(slide=>(
+            <SwiperSlide key={slide.id}>
             <div className="flex justify-around items-center bg-gray-100  w-full">
               <div className="text-center">
                 <h1 className="text-6xl font-bold font-poppins pt-3 pb-3 ">
-                  iPhone 15 Pro
+                  {slide.name}
                 </h1>
                 <p className="text-3xl font-poppins font-meduim mt-2 mb-2">
-                  Titanum, So strong. <br />
-                  So light, So Pro.
+                  {slide.short_description}
                 </p>
-
                 <p className="text-3xl font-poppins ">
                   <span className="line-through text-2xl text-gray-800 mr-5">
-                    $1,199
+                    ${slide.price}
                   </span>{" "}
-                  $1,099
+                  ${slide.discount_price}
                 </p>
-
                 <div className="flex justify-center items-center mt-5">
-                  <button className="border rounded-xl px-10 py-2 mr-3 bg-blue-700 text-white text-md">
+                  <button onClick={()=>navigate(`/products/${slide.id}`)} className="border rounded-xl px-10 py-2 mr-3 bg-blue-700 text-white text-md">
                     Buy
                   </button>
-                  <button className="border border-blue-700 rounded-xl px-8 py-2 text-md hover:bg-blue-700 hover:text-white text-black">
+                  <button onClick={()=>navigate(`/products/${slide.id}`)} className="border border-blue-700 rounded-xl px-8 py-2 text-md hover:bg-blue-700 hover:text-white text-black">
                     Learn More
                   </button>
                 </div>
               </div>
               <div>
-                <img src={iphone} alt="" className="w-100 h-100" />
+                <img src={slide.image} alt="" className="w-100 h-100" />
               </div>
             </div>
           </SwiperSlide>
-
-          <SwiperSlide>
-            <div className="flex justify-around items-center bg-gray-100  w-full">
-              <div className="text-center">
-                <h1 className="text-6xl font-bold font-poppins pt-3 pb-3 ">
-                  iPhone 15 Pro
-                </h1>
-                <p className="text-3xl font-poppins font-meduim mt-2 mb-2">
-                  Titanum, So strong. <br />
-                  So light, So Pro.
-                </p>
-
-                <p className="text-3xl font-poppins ">
-                  <span className="line-through text-2xl text-gray-800 mr-5">
-                    $1,199
-                  </span>{" "}
-                  $1,099
-                </p>
-
-                <div className="flex justify-center items-center mt-5">
-                  <button className="border rounded-xl px-10 py-2 mr-3 bg-blue-700 text-white text-md">
-                    Buy
-                  </button>
-                  <button className="border rounded-xl px-8 py-2 text-md hover:bg-blue-700 hover:text-white border-blue-700">
-                    Learn More
-                  </button>
-                </div>
-              </div>
-              <div>
-                <img src={iphone} alt="" className="w-100 h-100" />
-              </div>
-            </div>
-          </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </>
