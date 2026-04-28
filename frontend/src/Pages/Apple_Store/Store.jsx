62c09a63e24b73a8d838953c90b5161afa2cef0c
Jafar610 <jafarmohamed955@gmail.com>
2026-04-28 20:54:40 +0300

    21th commit

--- frontend/src/Pages/Apple_Store/Store.jsx
+++ frontend/src/Pages/Apple_Store/Store.jsx
@@ -3,15 +3,27 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper";
 import { Autoplay } from "swiper/modules";
 import imac from "../../assets/img/products/store-pro1.png";
-import iphonePro from '../../assets/img/products/store-phone-pro1.png'
-import ipad from '../../assets/img/products/store-pro3.png'
-import miniMac from '../../assets/img/products/store-pro4.png'
+import iphonePro from "../../assets/img/products/store-phone-pro1.png";
+import ipad from "../../assets/img/products/store-pro3.png";
+import miniMac from "../../assets/img/products/store-pro4.png";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function Store() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3002/products").then((res) => {
+      setProducts(res.data);
+    });
+  }, []);
+
   return (
     <>
       <div className="text-center mt-10">
         <div>
-          <h1 className="font-bold text-4xl font-poppins text-gray-600">Popular Product Catagories</h1>
+          <h1 className="font-bold text-4xl font-poppins text-gray-600">
+            Popular Product Catagories
+          </h1>
         </div>
 
         <div className="flex justify-between items-center ml-10 mr-10">
@@ -25,53 +37,26 @@ function Store() {
               disableOnInteraction: false,
             }}
           >
-            <SwiperSlide>
-              <div className="bg-gray-50 border border-gray-100 shadow-sm rounded-xl mt-8 text-center p-5 mr-3 ml-3 h-95">
-                <img src={imac} alt="" className="m-auto" />
-                <h1 className=" text-4xl text-center mt-5 mb-3 font-bold font-poppins">iMac</h1>
-                <p className="text-center text-md font-meduim">
-                  Advanced AI Performance and <br />
-                  game changing capabilities.
-                </p>
-
-                <p className="text-center text-xl font-bold mt-3 text-gray-500">$6,999</p>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div className="bg-gray-50 border border-gray-100 shadow-sm rounded-xl mt-8 text-center p-5 mr-3 ml-3 h-95">
-                <img src={iphonePro} alt="" className="h-45 m-auto"/>
-                <h1 className=" text-4xl text-center mt-5 mb-3 font-bold font-poppins">iPhone 17 pro</h1>
-                <p className="text-center text-md font-meduim">Titanium, So Strong, <br />So light, So Pro.</p>
-
-                <p className="text-center text-xl font-bold mt-3 text-gray-500">$6,999</p>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div className="bg-gray-50 border border-gray-100 shadow-sm rounded-xl mt-8 text-center p-5 mr-3 ml-3 h-95">
-                <img src={ipad} alt="" className="h-45 m-auto" />
-                <h1 className="text-4xl text-center mt-5 mb-3 font-bold font-poppins">iPad</h1>
-                <p className="text-center text-md font-meduim" >Super Charged by the Apple M2 chip.</p>
-                <p className="text-xl font-bold mt-3 text-gray-500">$999</p>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div className="bg-gray-50 border border-gray-100 shadow-sm rounded-xl mt-8 text-center p-5 mr-3 ml-3 h-95">
-                <img src={miniMac} alt="" className="h-50 m-auto" />
-                <h1 className="text-4xl text-center mb-3 font-bold font-poppins">Mini Mac</h1>
-                <p className="text-center text-md font-meduim">
-                  Powered alone <br />
-                  SuperPowered together.
-                </p>
-                <p className="text-xl font-bold mt-3 text-gray-500">$799</p>
-              </div>
-            </SwiperSlide>
-            
+            {products.map((product) => {
+              return (
+                <SwiperSlide key={product.id}>
+                  <div className="bg-gray-50 border border-gray-100 shadow-sm rounded-xl mt-8 text-center p-5 mr-3 ml-3 h-95">
+                    <img src={product.image} alt="" className="m-auto" />
+                    <h1 className=" text-4xl text-center mt-5 mb-3 font-bold font-poppins">
+                      {product.name}
+                    </h1>
+                    <p className="text-center text-md font-meduim">
+                      {product.short_description}
+                    </p>
+                    <p className="text-center text-xl font-bold mt-3 text-gray-500">
+                      ${product.price}
+                    </p>
+                  </div>
+                </SwiperSlide>
+              );
+            })}
           </Swiper>
         </div>
-
 
         <div className="flex justify-between items-center ml-10 mr-10 mb-15">
           <Swiper
@@ -87,31 +72,46 @@ function Store() {
             <SwiperSlide>
               <div className="bg-gray-50 border border-gray-100 shadow-sm rounded-xl mt-8 text-center p-5 mr-3 ml-3 h-95">
                 <img src={imac} alt="" className="m-auto" />
-                <h1 className=" text-4xl text-center mt-5 mb-3 font-bold font-poppins">iMac</h1>
+                <h1 className=" text-4xl text-center mt-5 mb-3 font-bold font-poppins">
+                  iMac
+                </h1>
                 <p className="text-center text-md font-meduim">
                   Advanced AI Performance and <br />
                   game changing capabilities.
                 </p>
 
-                <p className="text-center text-xl font-bold mt-3 text-gray-500">$6,999</p>
+                <p className="text-center text-xl font-bold mt-3 text-gray-500">
+                  $6,999
+                </p>
               </div>
             </SwiperSlide>
 
             <SwiperSlide>
               <div className="bg-gray-50 border border-gray-100 shadow-sm rounded-xl mt-8 text-center p-5 mr-3 ml-3 h-95">
-                <img src={iphonePro} alt="" className="h-45 m-auto"/>
-                <h1 className=" text-4xl text-center mt-5 mb-3 font-bold font-poppins">iPhone 17 pro</h1>
-                <p className="text-center text-md font-meduim">Titanium, So Strong, <br />So light, So Pro.</p>
+                <img src={iphonePro} alt="" className="h-45 m-auto" />
+                <h1 className=" text-4xl text-center mt-5 mb-3 font-bold font-poppins">
+                  iPhone 17 pro
+                </h1>
+                <p className="text-center text-md font-meduim">
+                  Titanium, So Strong, <br />
+                  So light, So Pro.
+                </p>
 
-                <p className="text-center text-xl font-bold mt-3 text-gray-500">$6,999</p>
+                <p className="text-center text-xl font-bold mt-3 text-gray-500">
+                  $6,999
+                </p>
               </div>
             </SwiperSlide>
 
             <SwiperSlide>
               <div className="bg-gray-50 border border-gray-100 shadow-sm rounded-xl mt-8 text-center p-5 mr-3 ml-3 h-95">
                 <img src={ipad} alt="" className="h-45 m-auto" />
-                <h1 className="text-4xl text-center mt-5 mb-3 font-bold font-poppins">iPad</h1>
-                <p className="text-center text-md font-meduim" >Super Charged by the Apple M2 chip.</p>
+                <h1 className="text-4xl text-center mt-5 mb-3 font-bold font-poppins">
+                  iPad
+                </h1>
+                <p className="text-center text-md font-meduim">
+                  Super Charged by the Apple M2 chip.
+                </p>
                 <p className="text-xl font-bold mt-3 text-gray-500">$999</p>
               </div>
             </SwiperSlide>
@@ -119,7 +119,9 @@ function Store() {
             <SwiperSlide>
               <div className="bg-gray-50 border border-gray-100 shadow-sm rounded-xl mt-8 text-center p-5 mr-3 ml-3 h-95">
                 <img src={miniMac} alt="" className="h-50 m-auto" />
-                <h1 className="text-4xl text-center mb-3 font-bold font-poppins">Mini Mac</h1>
+                <h1 className="text-4xl text-center mb-3 font-bold font-poppins">
+                  Mini Mac
+                </h1>
                 <p className="text-center text-md font-meduim">
                   Powered alone <br />
                   SuperPowered together.
