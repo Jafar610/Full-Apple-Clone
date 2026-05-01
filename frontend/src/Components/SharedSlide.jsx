9ebf88ad91288c670be73cb3d32d685b1e892dd1
Jafar610 <jafarmohamed955@gmail.com>
2026-05-01 23:09:17 +0300

    21th commit

--- frontend/src/Components/SharedSlide.jsx
+++ frontend/src/Components/SharedSlide.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Slide from '../Pages/Slide/Slide'
 import Store from '../Pages/Apple_Store/Store'
+import MacCategory from '../Pages/Apple_Store/MacCategory'
 
 function SharedSlide() {
   return (
     <>
       <Slide/>
       <Store/>
+      <MacCategory/>
     </>
   )
 }
--- frontend/src/Pages/Apple_Store/MacCategory.jsx
+++ frontend/src/Pages/Apple_Store/MacCategory.jsx
@@ -2,34 +2,29 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper";
 import { Autoplay } from "swiper/modules";
-import imac from "../../assets/img/products/store-pro1.png";
-import iphonePro from "../../assets/img/products/store-phone-pro1.png";
-import ipad from "../../assets/img/products/store-pro3.png";
-import miniMac from "../../assets/img/products/store-pro4.png";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 
-function Store() {
+function MacCategory() {
   const [products, setProducts] = useState([]);
-  useEffect(() => {
-    axios.get("http://localhost:3002/products").then((res) => {
+  useState(() => {
+    axios.get("http://localhost:3002/mac-category").then((res) => {
       setProducts(res.data);
     });
   }, []);
-
   return (
     <>
       <div className="text-center mt-10">
         <div>
           <h1 className="font-bold text-4xl font-poppins text-gray-600">
-            Popular Product Catagories
+            Popular Mac Products
           </h1>
         </div>
 
         <div className="flex justify-between items-center ml-10 mr-10">
           <Swiper
             modules={[Autoplay]}
-            slidesPerView={3}
+            slidesPerView={2}
             spaceBetween={10}
             loop={true}
             autoplay={{
@@ -40,8 +35,12 @@ function Store() {
             {products.map((product) => {
               return (
                 <SwiperSlide key={product.id}>
-                  <div className="bg-gray-50 border border-gray-100 shadow-sm rounded-xl mt-8 text-center p-5 mr-3 ml-3 h-95">
-                    <img src={product.image} alt="" className="m-auto w-40 h-40" />
+                  <div className="bg-gray-50 border border-gray-100 shadow-sm rounded-xl mt-8 text-center p-5 mr-3 ml-3 h-95 mb-10">
+                    <img
+                      src={product.image}
+                      alt=""
+                      className="m-auto w-40 h-40"
+                    />
                     <h1 className=" text-4xl text-center mt-5 mb-3 font-bold font-poppins">
                       {product.name}
                     </h1>
@@ -51,16 +50,30 @@ function Store() {
                     <p className="text-center text-xl font-bold mt-3 text-gray-500">
                       ${product.price}
                     </p>
+
+                    <div className="flex justify-center items-center mt-2 mb-5">
+                      <button
+                        onClick={() => navigate(`/products/${slide.id}`)}
+                        className="border rounded-xl px-10 py-2 mr-3 bg-blue-700 text-white text-md"
+                      >
+                        Buy
+                      </button>
+                      <button
+                        onClick={() => navigate(`/products/${slide.id}`)}
+                        className="border border-blue-700 rounded-xl px-8 py-2 text-md hover:bg-blue-700 hover:text-white text-black"
+                      >
+                        Learn More
+                      </button>
+                    </div>
                   </div>
                 </SwiperSlide>
               );
             })}
           </Swiper>
         </div>
-
       </div>
     </>
   );
 }
 
-export default Store;
+export default MacCategory;
